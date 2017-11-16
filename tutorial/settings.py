de412505f411175f897fe9fb6b5e7b87f6c11a8a
piping <piping>
2017-11-17 00:44:54 +0800

    first commit

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -89,7 +89,7 @@ HTTPCACHE_ENABLED = True
 HTTPCACHE_EXPIRATION_SECS = 0
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
-HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 
 HTTPERROR_ALLOW_ALL = True
